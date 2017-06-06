d68c6bd6efb3cd9cca1fa5c58b36ad4c66e16154
Katarina <dordevic.katarina@outlook.com>
2017-06-06 13:10:11 -0700

    Added Grid View

--- app.js
+++ app.js
@@ -1,33 +1,50 @@
 var app = angular.module('app', []);
-app.controller('ctrl1', function($scope){
+app.controller('ctrlCard1', function ($scope) {
     $scope.animate = '';
 
-    $scope.toggle = function(){
+    $scope.toggle = function () {
         $scope.animate = $scope.animate === 'open' ? '' : 'open';
     }
 })
 
-app.controller('ctrl2', function($scope){
+app.controller('ctrlCard2', function ($scope) {
     $scope.animate = '';
 
-    $scope.toggle = function(){
+    $scope.toggle = function () {
         $scope.animate = $scope.animate === 'open' ? '' : 'open';
     }
 })
 
-app.controller('ctrl3', function($scope){
+app.controller('ctrlCard3', function ($scope) {
     $scope.animate = '';
 
-    $scope.toggle = function(){
+    $scope.toggle = function () {
         $scope.animate = $scope.animate === 'open' ? '' : 'open';
     }
 })
 
 
-app.controller('ctrl4', function($scope){
+app.controller('ctrlCard4', function ($scope) {
     $scope.animate = '';
 
-    $scope.toggle = function(){
+    $scope.toggle = function () {
         $scope.animate = $scope.animate === 'open' ? '' : 'open';
     }
+})
+
+app.controller('ctrlGridView', function ($scope) {
+    $scope.tasks = [];
+
+    var task1 = { id: 1, name: 'Task 1', desc: 'Ovo je Task 1' };
+    var task2 = { id: 2, name: 'Task 2', desc: 'Ovo je Task 2' };
+    var task3 = { id: 3, name: 'Task 3', desc: 'Ovo je Task 3' };
+    var task4 = { id: 4, name: 'Task 4', desc: 'Ovo je Task 4' };
+    var task5 = { id: 5, name: 'Task 5', desc: 'Ovo je Task 5' };
+
+    $scope.tasks.push(task1);
+    $scope.tasks.push(task2);
+    $scope.tasks.push(task3);
+    $scope.tasks.push(task4);
+    $scope.tasks.push(task5);
+
 })
